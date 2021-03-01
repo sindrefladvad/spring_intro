d0e49c906bbdf176c52481efb977b2a4efc5456a
sindrefladvad <sindrefladvad@gmail.com>
2021-03-01 15:08:28 +0100

    la til systemproperties

---
+++ src/test/java/oslometh/webprogrammering/WebprogrammeringApplicationTests.java
@@ -0,0 +1,13 @@
+package oslometh.webprogrammering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprogrammeringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
