e11232bd49a03d13a6850c1b57637a591539c3c9
sindrefladvad <sindrefladvad@gmail.com>
2021-03-01 14:53:23 +0100

    Initial commit

---
+++ src/main/java/oslometh/webprogrammering/HeiController.java
@@ -0,0 +1,13 @@
+package oslometh.webprogrammering;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HeiController {
+    @GetMapping("/")
+    public String hei(String navn){
+        return "Hei verden "+ navn;
+    }
+}
